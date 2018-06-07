31ce6b61c92839c40f3364848f218bf25b24d8d2
wuj <wuj@avit.org.cn>
2018-06-07 11:41:55 +0800

    init

---
+++ dist/static/lang/en.js
@@ -0,0 +1,15 @@
+import enLocale from 'element-ui/lib/locale/lang/en'
+export const m = {
+  username: 'username',
+  usernameHold: 'please input your account',
+  usernameWarn: 'please input your account',
+  passwd: 'password',
+  passwdHold: 'please input your password',
+  passwdWarn: 'please input your password',
+  login: 'sign in',
+  logoff: 'sign out',
+  onLogin: 'logining……',
+  requestError: 'Request Error',
+  userError: 'username or password error',
+  enLocale
+}
